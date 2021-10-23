084e8e26796823b8fa6dbaea470992c36c731a60
yong <yong@yongdeMacBook-Air.local>
2021-10-23 22:30:07 +0800

    database/sql

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"gitee.com/superwhys/GinTest/mysqlInit"
+	mysqlSearch "gitee.com/superwhys/GinTest/mysql_search_demo"
+	"log"
+)
+
+func main() {
+
+	var db *sql.DB
+
+	dsn := "root:yang4869@tcp(localhost:3306)/sql_test"
+
+	if err := mysqlinit.InitMysql(dsn); err != nil {
+		log.Fatalln(err)
+	}
+
+	db = mysqlinit.Db
+	defer db.Close()
+
+
+	sqlStr := "select id, name, age from user where id=?"
+	mysqlSearch.QueryRowDemo(sqlStr, db)
+	fmt.Println("search more rows")
+	sqlStr1 := "select id, name, age from user where id>?"
+	mysqlSearch.QueryMultiRowDemo(sqlStr1, db)
+
+}
---
+++ mysql_search_demo/mysqlSearch.go
@@ -0,0 +1,28 @@
+package mysqlSearch
+
+import (
+	"database/sql"
+	"fmt"
+	mysqlinit "gitee.com/superwhys/GinTest/mysqlInit"
+)
+
+func QueryMultiRowDemo(sqlStr string, db *sql.DB) {
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("quert faild, err: %v\n", err)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var u mysqlinit.User
+		err = rows.Scan(&u.Id, &u.Name, &u.Age)
+		if err != nil {
+			fmt.Printf("scan faild, err: %v\n", err)
+			return
+		}
+
+		fmt.Printf("id: %d, name: %s, age: %d\n", u.Id, u.Name, u.Age)
+	}
+}
