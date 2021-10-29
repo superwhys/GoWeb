1e4e1942e4588a09681cebc8084e100b48e18986
Superyong <269085434@qq.com>
2021-10-29 21:04:16 +0800

    sqlPrepare

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"gitee.com/superwhys/GinTest/mysqlInit"
 	mysqlSearch "gitee.com/superwhys/GinTest/mysql_search_demo"
+	"gitee.com/superwhys/GinTest/mysqlprepare"
 	"log"
 )
 
@@ -21,12 +22,16 @@ func main() {
 	db = mysqlinit.Db
 	defer db.Close()
 
-
+	fmt.Println("queryRow")
 	sqlStr := "select id, name, age from user where id=?"
 	mysqlSearch.QueryRowDemo(sqlStr, db)
 
+	fmt.Println("queryMultiRow")
 	fmt.Println("search more rows")
 	sqlStr1 := "select id, name, age from user where id>?"
 	mysqlSearch.QueryMultiRowDemo(sqlStr1, db)
 
+	fmt.Println("MysqlPrepare")
+	sqlStr2 := "select id, name, age from user where id>?"
+	mysqlprepare.MysqlPrepareDemo(sqlStr2, db)
 }
---
+++ mysqlprepare/mysqlPrepare.go
@@ -0,0 +1,33 @@
+package mysqlprepare
+
+import (
+	"database/sql"
+	"fmt"
+	mysqlinit "gitee.com/superwhys/GinTest/mysqlInit"
+)
+
+func MysqlPrepareDemo(sqlStr string, db *sql.DB) {
+	stmt, err := db.Prepare(sqlStr)
+	if err != nil {
+		fmt.Printf("prepare failed, err: %v\n", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(0)
+	if err != nil {
+		fmt.Printf("query failed, err: %v\n", err)
+	}
+	defer rows.Close()
+	// 循环读取结果集中的数据
+
+	for rows.Next() {
+		var u mysqlinit.User
+		err := rows.Scan(&u.Id, &u.Name, &u.Age)
+		if err != nil {
+			fmt.Printf("scan faild, err: %v\n", err)
+		}
+		fmt.Printf("id: %d, name: %s, age: %d\n", u.Id, u.Name, u.Age)
+	}
+
+}
+
